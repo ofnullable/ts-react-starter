35c123394e3e3c204920850314f2f666a590d97f
ofnullable <ofnullable@gmail.com>
2020-04-23 23:11:43 +0900

    :lipstick: Update styles with emotion, scss

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react';
 
 const HomePage = () => {
-  return <div>Home page!|>|>!</div>;
+  return (
+    <div>
+      <h1>Home page!|>|>!</h1>
+    </div>
+  );
 };
 
 export default HomePage;
