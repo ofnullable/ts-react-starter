bce002bfa6d4dd64f24e0e8aba977fea7f0cdaa4
ofnullable <ofnullable@gmail.com>
2020-04-27 01:34:25 +0900

    :heavy_plus_sign: Add @babel/runtime and update configuration

--- babel.config.js
+++ babel.config.js
@@ -4,23 +4,20 @@ module.exports = (api) => {
     presets: [
       '@babel/preset-react',
       '@babel/preset-typescript',
-      [
-        '@babel/preset-env',
-        {
-          corejs: { version: 3, proposals: true },
-          useBuiltIns: 'usage',
-          modules: webpack ? false : 'commonjs',
-        },
-      ],
+      ['@babel/preset-env', {
+        corejs: { version: 3, proposals: true },
+        useBuiltIns: 'usage',
+        modules: webpack ? false : 'commonjs',
+      }],
     ],
     plugins: [
       '@loadable/babel-plugin',
-      [
-        'babel-plugin-emotion',
-        {
-          sourceMap: true,
-        },
-      ],
+      ['babel-plugin-emotion', {
+        sourceMap: true,
+      }],
+      ['@babel/transform-runtime', {
+        corejs: { version: 3, proposals: true }
+      }],
     ],
   };
 };
